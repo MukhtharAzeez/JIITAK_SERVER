5a284ba8d2e01055fbb8be8a1ac3cc01a313c778
MukhtharAzeez <mukhtharazeez28@gmail.com>
2023-04-15 16:13:37 +0530

    build api is almost completed

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,46 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './models/user.dto';
+import { UserEntity } from './models/post.entity';
+import { UpdateUserDto } from './models/updateUser.dto';
+import { UpdatePasswordDto } from './models/updatePassword.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('/signup')
-  signup(@Body() userDto: UserDto): void {
+  signup(@Body() userDto: UserDto): Promise<UserEntity> {
     return this.userService.signup(userDto);
+  }
+
+  @Post('/login')
+  login(@Body() userDto: UserDto) {
+    return this.userService.login(userDto);
+  }
+
+  @Patch('/update/:id')
+  update(@Param('id') id, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateProfile(
+      id,
+      updateUserDto.email,
+      updateUserDto.username,
+    );
+  }
+
+  @Patch('/updatePassword/:id')
+  updatePassword(
+    @Param('id') id,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ) {
+    return this.userService.updatePassword(id, updatePasswordDto);
+  }
+
+  @Patch('/forgotPassword/:id')
+  forgotPassword(
+    @Param('id') id,
+    @Body() updatePasswordDto: { newPassword: string },
+  ) {
+    return this.userService.forgotPassword(id, updatePasswordDto.newPassword);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserDto } from './models/user.dto';
 import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { UserEntity } from './models/post.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as argon2 from 'argon2';
+import { UpdatePasswordDto } from './models/updatePassword.dto';
 
 @Injectable()
 export class UserService {
@@ -12,7 +14,90 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
-  signup(userDto: UserDto) {
-    console.log('Hello');
+  async signup(userDto: UserDto): Promise<UserEntity> {
+    const userExists = await this.userRepository.findOne({
+      where: { email: userDto.email },
+    });
+    if (userExists) {
+      throw new HttpException(
+        'User already exists with this email',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    userDto.password = await argon2.hash(userDto.password);
+    return this.userRepository.save(userDto);
+  }
+
+  async login(userDto: UserDto) {
+    const checkUser = await this.userRepository.findOne({
+      where: { email: userDto.email },
+    });
+    if (checkUser) {
+      const passwordCheck = await argon2.verify(
+        checkUser.password,
+        userDto.password,
+      );
+      if (passwordCheck) {
+        return checkUser;
+      } else {
+        throw new HttpException('Invalid credentials', HttpStatus.BAD_REQUEST);
+      }
+    }
+  }
+
+  async updateProfile(id: number, email: string, username: string) {
+    const queryBuilder = this.userRepository
+      .createQueryBuilder('user')
+      .update(UserEntity)
+      .set({ email, username })
+      .where('id = :id', { id })
+      .returning('*');
+    const result = await queryBuilder.execute();
+    return result.raw[0];
+  }
+
+  async updatePassword(id: number, updatePasswordDto: UpdatePasswordDto) {
+    const checkUser = await this.userRepository.findOne({
+      where: { id: id },
+    });
+    if (checkUser) {
+      const passwordCheck = await argon2.verify(
+        checkUser.password,
+        updatePasswordDto.oldPassword,
+      );
+
+      if (!passwordCheck) {
+        throw new HttpException(
+          'Old password is wrong',
+          HttpStatus.BAD_REQUEST,
+        );
+      } else {
+        const password = await argon2.hash(updatePasswordDto.newPassword);
+
+        const queryBuilder = this.userRepository
+          .createQueryBuilder('user')
+          .update(UserEntity)
+          .set({ password })
+          .where('id = :id', { id })
+          .returning('*');
+        const result = await queryBuilder.execute();
+        return result.raw[0];
+      }
+    } else {
+      return null;
+    }
+  }
+
+  async forgotPassword(id: number, newPassword: string) {
+    const password = await argon2.hash(newPassword);
+
+    const queryBuilder = this.userRepository
+      .createQueryBuilder('user')
+      .update(UserEntity)
+      .set({ password })
+      .where('id = :id', { id })
+      .returning('*');
+    const result = await queryBuilder.execute();
+    return result.raw[0];
   }
 }
